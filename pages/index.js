627a66ad9632759f808c97d5a4486f3ed8597df0
emothek <mokhtar.developer@gmail.com>
2021-05-26 17:40:29 +0100

    first next app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <img src='http://www.viamigo.com/img/l/guide/4220/megherbi-mokhtar.jpg' />
+        </div>
+    )
+}
+
+export default Home;
